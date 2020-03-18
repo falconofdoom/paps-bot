8852728c3cd4991c2a0508b5eb475628fc4bad63
Redan Benedict Alcaide <aredben@pop-os.localdomain>
2020-03-19 00:53:03 +0800

    Removed debugging logs Signed-off-by: Redan Benedict Alcaide <aredben@pop-os.localdomain>

--- penge.sh
+++ penge.sh
@@ -3,16 +3,9 @@ COMMAND=$(echo $REQUEST | awk '{print $1}' | tr '[:upper:]' '[:lower:]')
 OPTIONS=$(echo $REQUEST | cut -d " " -f2- | sed 's/https\?:\/\///g')
 USER=$2
 
-echo "TEST"
-echo "$AUTH_USER"
-echo "TESTEND"
-
 if [ $COMMAND = "jwt" ]; then
-	token=`curl -X POST -H "Content-Type: application/json" -d '{"username":"$AUTH_USER", "password":"$AUTH_PASSWORD"}' "https://nis1.alnaghi.com:7506/AuthenticationService/rest/auth/"`
-	token=`echo $token | jq .token`
-	echo "JWT Token generated:" 
-	echo "================================================================================================================="
-	echo "$token" 
+	response=`curl -X POST -H "Content-Type: application/json" -d '{"username":"$AUTH_USER", "password":"$AUTH_PASSWORD"}' "https://nis1.alnaghi.com:7506/AuthenticationService/rest/auth/"`
+	echo "$response" 
 else 
 	echo "Ayoko nga. Mali2 ka naman."
 fi
