3cc88b1d92d67e38fa8f340d8ff11a5a1b4242ed
Redan Benedict Alcaide <aredben@pop-os.localdomain>
2020-03-19 00:58:45 +0800

    Ito talaga. Signed-off-by: Redan Benedict Alcaide <aredben@pop-os.localdomain>

--- penge.sh
+++ penge.sh
@@ -3,8 +3,9 @@ COMMAND=$(echo $REQUEST | awk '{print $1}' | tr '[:upper:]' '[:lower:]')
 OPTIONS=$(echo $REQUEST | cut -d " " -f2- | sed 's/https\?:\/\///g')
 USER=$2
 
+
 if [ $COMMAND = "jwt" ]; then
-	response=`curl -X POST -H "Content-Type: application/json" -d '{"username":"$AUTH_USER", "password":"$AUTH_PASSWORD"}' "https://nis1.alnaghi.com:7506/AuthenticationService/rest/auth/"`
+	response=`curl -X POST -H "Content-Type: application/json" -d "{\"username\":\"$AUTH_USER\", \"password\":\"$AUTH_PASSWORD\"}" "https://nis1.alnaghi.com:7506/AuthenticationService/rest/auth/"`
 	echo "$response" 
 else 
 	echo "Ayoko nga. Mali2 ka naman."
